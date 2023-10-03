67c5aeb814deff63c396d6d07d2ac77dd3ed6178
Yoav Green <yoavgreen15@gmail.com>
2023-10-03 15:42:38 +0300

    Socket refactor

--- Socket/custom_socket.py
+++ Socket/custom_socket.py
@@ -4,19 +4,31 @@ from threading import Thread
 
 class CustomSocket(Thread):
 
-    def __init__(self, socket_ip: str, socket_port: int, connection_protocol: str) -> None:
-        
+    def __init__(self, connection_protocol: str) -> None:
+        super().__init__()
+        self.connection_protocol = connection_protocol
 
-    def create_server_socket(self, connection_protocol: str) -> socket:
+    def set_socket_protocol(self, connection_protocol: str):
+        if connection_protocol == "tcp":
+            return SOCK_STREAM
+        elif connection_protocol == "udp":
+            return SOCK_DGRAM
+        else:
+            raise ValueError(f"Unsupported connection protocol '{connection_protocol}'.")
+
+    def create_socket(self) -> socket:
         try:
-            protocol = self.set_socket_protocol(connection_protocol)
-            # TODO - validate socket creation
-            server_socket = socket(AF_INET, protocol)
+            protocol = self.set_socket_protocol(self.connection_protocol)
+            custom_socket = socket(AF_INET, protocol)
 
-            return socket
+            return custom_socket
 
         except socket_error as err:
             raise CustomSocketError(f"Unable to create socket, Error: {err}.")
+
+    def setup(self) -> None:
+        """Override method to setup server/client"""
+        pass
 
 
 class CustomSocketError(Exception):
