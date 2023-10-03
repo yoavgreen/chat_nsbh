88d01b992f4024f9f1b2ebe1f626b197e746cd43
Yoav Green <yoavgreen15@gmail.com>
2023-10-03 13:16:37 +0300

    Server Refactor

--- Server/server_logic.py
+++ Server/server_logic.py
@@ -1,58 +1,65 @@
 from socket import socket, AF_INET, SOCK_STREAM, SOCK_DGRAM, error as socket_error
+from Socket.custom_socket import CustomSocket
 import threading
 
-class Server:
-	def __init__(self, server_ip: str, server_port: int, connection_protocol: str) -> None:
-		self.server_ip = server_ip
-		self.server_port = server_port
-		# Create server socket in the constructor
-		self.connected_clients = []
-		self.server_socket = self.create_server_socket(connection_protocol)
 
-	def set_server_protocol(self, connection_protocol: str):
-		if connection_protocol == "tcp":
-			return SOCK_STREAM
-		elif connection_protocol == "udp":
-			return SOCK_DGRAM
-		else:
-			raise ValueError(f"Unsupported connection protocol '{connection_protocol}'.")
+class Server(CustomSocket):
+    def __init__(self, server_ip: str, server_port: int, connection_protocol: str) -> None:
+        super().__init__(connection_protocol)
+        self.server_ip = server_ip
+        self.server_port = server_port
 
-	def create_server_socket(self, connection_protocol) -> socket:
-		try:
-			protocol = self.set_server_protocol(connection_protocol)
-			# TODO - validate socket creation
-			server_socket = socket(AF_INET, protocol)
-			
-			return server_socket 	
+        # Create server socket in the constructor
+        self.connected_clients = []
+        self.active_connections = 0
+        self.server_socket = super().create_socket()
 
-		except socket_error as err:
-			raise ServerError(f"Unable to create server socket, Error: {err}.")
+    def setup_server(self) -> None:
+        try:
+            self.server_socket.bind((str(self.server_ip), self.server_port))
+            self.server_socket.listen()
 
+        except socket_error as err:
+            raise ServerError(f"Unable to setup server, Error: '{err}'.")
 
-	def setup_server(self) -> None:	
-		try:
-			self.server_socket.bind((str(self.server_ip), self.server_port))
-			self.server_socket.listen()
-			print(f"Server is now listening on {self.server_ip}:{self.server_port}...")	
+    def run(self):
+        try:
+            self.setup_server()
+            print(ServerConstants.SERVER_ART_LOGO, end="\n\n")
+            print(f"Server is now listening on {self.server_ip}:{self.server_port}...")
 
-		except socket_error as err:
-			raise ServerError(f"Unable to setup server, Error: '{err}'.")
+            while True:
+                connection, address = self.server_socket.accept()
+                print(f"Server connected to {str(address)}")
+
+        except Exception as err:
+            raise ServerError(f"Unable to setup server, Error: {err}.")
 
 
 class ServerConstants:
-	
-	CONFIG_FILE_NAME = "Server/server_config.json"
-	
-	SERVER_IP_ADDRESS = "server_ip_address"
-	SERVER_PORT = "server_port"
-	SERVER_PROTOCOL = "server_protocol"
+    CONFIG_FILE_NAME = "server_config.json"
 
-	config_file_template = {
-		SERVER_IP_ADDRESS: "127.0.0.1",
-		SERVER_PORT: 8888,
-		SERVER_PROTOCOL: "tcp"
-	}
+    SERVER_IP_ADDRESS = "server_ip_address"
+    SERVER_PORT = "server_port"
+    SERVER_PROTOCOL = "server_protocol"
+
+    config_file_template = {
+        SERVER_IP_ADDRESS: "127.0.0.1",
+        SERVER_PORT: 8888,
+        SERVER_PROTOCOL: "tcp"
+    }
+
+    SERVER_ART_LOGO = """
+    
+       ____ _           _   _  __       
+      / ___| |__   __ _| |_(_)/ _|_   _ 
+     | |   | '_ \ / _` | __| | |_| | | |
+     | |___| | | | (_| | |_| |  _| |_| |
+      \____|_| |_|\__,_|\__|_|_|  \__, |
+                                  |___/ 
+    
+        """
 
 
 class ServerError(Exception):
-	pass
+    pass
